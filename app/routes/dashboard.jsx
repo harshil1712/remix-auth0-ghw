6d7e70e9eee96a50781a3c3b7ed3526567469cfc
Harshil <ghagrawal17@gmail.com>
2023-06-11 14:47:55 +0200

    ⚡Render user information

--- app/routes/dashboard.jsx
+++ app/routes/dashboard.jsx
@@ -1,20 +1,23 @@
-import { Form } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
 import { authenticator } from "../utils/auth.server";
+import { json } from "@remix-run/node";
 
 export const loader = async({request}) => {
-	return await authenticator.isAuthenticated(request,{
+	const user = await authenticator.isAuthenticated(request, {
 		failureRedirect: '/auth/login'
-	})
+	});
+	return json({user})
 }
 
 export default function Dashboard() {
+	const {user} = useLoaderData();
 	return (
 		<div>
-			<h1>Hello</h1>
-			<h2>Successfuly logged in!</h2>
+			<h1>Hello {user.displayName}</h1>
 			<Form action="/auth/logout" method="POST">
 				<button>Logout</button>
 			</Form>
+			<img src={user.photos[0].value} alt={`Avatar of ${user.displayName}`} />
 		</div>
 	)
 }
